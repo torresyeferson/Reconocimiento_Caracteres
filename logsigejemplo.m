2d1e7464b1f96a87fd573133fd31108e0d7f4cfa
torresyeferson <yeferson.torres11@gmail.com>
2014-11-04 11:54:45 -0500

    Add

--- logsigejemplo.m
+++ logsigejemplo.m
@@ -1,3 +1,8 @@
 [alfabeto,salidas]=prprob;
 clasificador= newff(minmax(alfabeto),[20 26],{'tansig','logsig'},'trainlm');
 [clasificador]= train(clasificador,alfabeto,salidas);
+plotchar(alfabeto(:,1))%para imprimir la letra A
+A=sim(alfabeto,clasificador);%matriz de resultados con margen de error
+%para probar con la entrada de un solo caracter
+%entrada=[0 1 0 1 1 1 0 1 0 1 1 1 1 1 0 0 1 1 1 1 1 1 0 1 1 0 0 0 0 1 1 1 1 1 1]
+%A=sim(clasificador,entrada)
